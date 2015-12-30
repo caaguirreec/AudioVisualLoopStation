6b5ac665bce981371a49fecc5bd68e56537ecf29
Gogs <gogs@fake.local>
2015-12-29 20:36:05 -0500

    First real approach to looping with audio. Works for wav files and for now, only for one recording.

--- AudioVisualLoopStation/AudioVisualLoopStation/src/audiovisualloopstation/Main.java
+++ AudioVisualLoopStation/AudioVisualLoopStation/src/audiovisualloopstation/Main.java
@@ -18,16 +18,24 @@ import it.sauronsoftware.jave.EncoderException;
 import it.sauronsoftware.jave.EncodingAttributes;
 import java.awt.Color;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.sound.sampled.AudioFileFormat;
 import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
+import javax.sound.sampled.Clip;
 import javax.sound.sampled.DataLine;
+import javax.sound.sampled.Line;
 import javax.sound.sampled.LineUnavailableException;
 import javax.sound.sampled.TargetDataLine;
+import javax.sound.sampled.UnsupportedAudioFileException;
+import sun.audio.AudioPlayer;
+import sun.audio.AudioStream;
 
 /**
  *
@@ -41,11 +49,13 @@ public class Main extends javax.swing.JFrame {
 AudioFileFormat.Type aFF_T = AudioFileFormat.Type.WAVE;
 AudioFormat aF = new AudioFormat(8000.0F, 16, 1, true, false);
 TargetDataLine tD;
-String RecordName;
+int RecordName;
+
 boolean record=false;
 int i;
 int j;
 int modu;
+int ctrLoop;
 
  /**
   * Thread to record
@@ -62,6 +72,45 @@ AudioSystem.write(new AudioInputStream(tD), aFF_T, f);
 }catch (Exception e){}
 }
 }
+ class LoopThread extends Thread {
+          @Override
+          public void run() {
+          while(true)
+          {System.out.println("Looping running.");
+              startlooping();}
+          }
+          public void startlooping(){
+              String fileName; 
+              if(RecordName==1)
+               { fileName = "/home/pi/NetBeansProjects/AudioVisualLoopStation/dist/recordings/"+(RecordName)+".wav";}
+               else
+               { fileName = "/home/pi/NetBeansProjects/AudioVisualLoopStation/dist/recordings/"+(RecordName-ctrLoop)+".wav";}
+               InputStream in = null;
+                try {
+                    in = new FileInputStream(fileName);
+                   } catch (FileNotFoundException e) {
+                    System.out.println("Media file not present in C drive.");
+                    e.printStackTrace();
+                   }
+                    AudioStream as = null;
+               try {
+                    as = new AudioStream(in);
+               } catch (IOException e) {
+                   e.printStackTrace();
+               }
+               AudioPlayer.player.start(as);
+   try {
+        do {
+        } while (as.available() > 0);
+        
+            //startlooping();
+        
+    } catch (IOException ex) {
+        System.out.println("Looping problem.");
+    }
+
+  }
+          }  
 class CapThreadStop extends Thread {
 @Override
 public void run() {
@@ -107,6 +156,7 @@ public void run() {
 
         jPanel1 = new javax.swing.JPanel();
         jLabel1 = new javax.swing.JLabel();
+        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         addWindowListener(new java.awt.event.WindowAdapter() {
@@ -130,6 +180,13 @@ public void run() {
 
         jLabel1.setText("jLabel1");
 
+        jButton1.setText("Play");
+        jButton1.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jButton1ActionPerformed(evt);
+            }
+        });
+
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
         layout.setHorizontalGroup(
@@ -141,7 +198,9 @@ public void run() {
             .addGroup(layout.createSequentialGroup()
                 .addGap(168, 168, 168)
                 .addComponent(jLabel1)
-                .addContainerGap(198, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 119, Short.MAX_VALUE)
+                .addComponent(jButton1)
+                .addGap(26, 26, 26))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -149,8 +208,10 @@ public void run() {
                 .addContainerGap()
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jLabel1)
-                .addContainerGap(25, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel1)
+                    .addComponent(jButton1))
+                .addContainerGap(16, Short.MAX_VALUE))
         );
 
         pack();
@@ -161,7 +222,7 @@ public void run() {
        // (new File("/home/pi/NetBeansProjects/AudioVisualLoopStation/dist"+"/recordings/")).mkdirs();
         final GpioController gpio = GpioFactory.getInstance();
         i=0;
-        
+        ctrLoop=0;
          
         final GpioPinDigitalInput myButton = gpio.provisionDigitalInputPin(RaspiPin.GPIO_02,PinPullResistance.PULL_DOWN);
             // create and register gpio pin listener
@@ -172,13 +233,14 @@ public void run() {
             
             // display pin state on console
             System.out.println(" --> GPIO PIN STATE CHANGE: " + event.getPin() + " = " + event.getState());
-            //if("HIGH".equals(event.getState().toString()))
+           
             if("HIGH".equals(event.getState().toString()) && modu==0) 
-            {jLabel1.setText(event.getState().toString());
-            jPanel1.setBackground(Color.red);
+            {   i++;
+                jLabel1.setText("Start Recording: "+i+".wav");
+                jPanel1.setBackground(Color.red);
              
-             i++;
-             RecordName=Integer.toString(i);
+             
+             RecordName=i;
              DataLine.Info dLI = new DataLine.Info(TargetDataLine.class,aF);
                 try {
                     tD = (TargetDataLine)AudioSystem.getLine(dLI);
@@ -192,28 +254,10 @@ public void run() {
             ////////////////////////////////////////////////////////
             //if("LOW".equals(event.getState().toString()))
             if("HIGH".equals(event.getState().toString()) && modu!=0) 
-            {jLabel1.setText(event.getState().toString());
+            {jLabel1.setText("Stop Recording: "+i+".wav");
             jPanel1.setBackground(Color.WHITE);
             tD.close();
-//            File source = new File("/home/pi/NetBeansProjects/AudioVisualLoopStation/dist/recordings/"+RecordName+".wav");
-//            File target = new File("/home/pi/NetBeansProjects/AudioVisualLoopStation/dist/recordings/"+RecordName+".mp3");
-//            AudioAttributes audio = new AudioAttributes();
-//            audio.setCodec("libmp3lame");
-//            audio.setBitRate(new Integer(128000));
-//            audio.setChannels(new Integer(2));
-//            audio.setSamplingRate(new Integer(44100));
-//            EncodingAttributes attrs = new EncodingAttributes();
-//            attrs.setFormat("mp3");
-//            attrs.setAudioAttributes(audio);
-//            Encoder encoder = new Encoder();
-//             try {
-//                encoder.encode(source, target, attrs);
-//                 } catch (IllegalArgumentException ex) {
-//       
-//             } catch (EncoderException ex) {
-//      
-//             }
-             //source.delete();
+           
             System.out.println("Grabación finalizada!"+modu);
             
             j++;
@@ -230,7 +274,15 @@ public void run() {
 //            }
 //        }
     }//GEN-LAST:event_formWindowOpened
-  
+
+    private void jButton1ActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jButton1ActionPerformed
+        // TODO add your handling code here:
+       
+        new LoopThread().start(); 
+        ctrLoop++;
+              
+    }//GEN-LAST:event_jButton1ActionPerformed
+         
   
     /**
      * @param args the command line arguments
@@ -268,6 +320,7 @@ public void run() {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JButton jButton1;
     private javax.swing.JLabel jLabel1;
     private javax.swing.JPanel jPanel1;
     // End of variables declaration//GEN-END:variables
